0eed3f8f946598553979243ce955dce049854850
sevku <severin.kunz@gmail.com>
2017-12-06 23:22:20 +0100

    Merge branch 'master' into gui
    gui update

--- src/gui/GUITest.java
+++ src/gui/GUITest.java
@@ -5,22 +5,7 @@ import javax.swing.*;
 public class GUITest {
 
 	private static void createAndShowGUI() {
-        /*//Create and set up the window.
-        JFrame frame = new JFrame("HelloWorldSwing");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Add the ubiquitous "Hello World" label.
-        JLabel label = new JLabel("Hello World");
-        frame.getContentPane().add(label);
-
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);*/
-		
-		JFrame f = new JFrame( "Hallo Welt" );
-		// Macht einen JFrame sichtbar
-		f.setResizable(true);
-		f.setVisible(true);
+        MainFrame frame = new MainFrame("Mobility Rechner");
     }
 	
 	public static void main(String[] args) {
--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,9 +1,46 @@
 package gui;
 
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+
 public class MainFrame extends javax.swing.JFrame {
 	private static final long serialVersionUID = 1L;
 
-
+	public MainFrame(final String title) {
+		super(title);
+		
+		
+		setLayout(new BorderLayout());
+		
+		// padding
+		JPanel contentPanel = new JPanel();
+		Border padding = BorderFactory.createEmptyBorder(10, 10, 10, 10);
+		contentPanel.setBorder(padding);
+		setContentPane(contentPanel);
+		
+		JTextField input = new JTextField(5);
+        input.setBounds(100,20,100,20);
+        add(input);
+		
+        
+        setLayout(new BorderLayout());
+        
+        JButton button = new JButton("Click Me");
+        add(button, BorderLayout.SOUTH);
+		
+        
+        
+        
+        
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(300, 400);
+		setResizable(false);
+		
+		setVisible(true);
+	}
 
 
 
