ba370182e5f636daf01f59094e641d841f02e4d6
sevku <severin.kunz@gmail.com>
2017-12-06 19:37:47 +0100

    package gui

---
+++ src/gui/GuiTest.java
@@ -0,0 +1,18 @@
+package gui;
+
+public class GuiTest {
+
+	
+	public static void main(String[] args) {
+		
+		MainFrame frame = new MainFrame();
+		frame.setTitle("Mobility Rechner");
+		frame.setSize(300,400);
+		frame.setResizable(false);
+		frame.setLocation(50,50);
+		frame.setVisible(true);
+		
+	}
+	
+	
+}
---
+++ src/gui/MainFrame.java
@@ -0,0 +1,12 @@
+package gui;
+
+public class MainFrame extends javax.swing.JFrame {
+	private static final long serialVersionUID = 1L;
+
+
+
+
+
+
+
+}
