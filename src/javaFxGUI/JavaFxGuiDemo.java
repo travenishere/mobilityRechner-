85d09dafb55369a8467e9b4e02bf420d7c76e6cc
sevku <severin.kunz@gmail.com>
2017-12-07 17:46:38 +0100

    first working java fx stage window

--- src/javaFxGUI/JavaFxGuiDemo.java
+++ src/javaFxGUI/JavaFxGuiDemo.java
@@ -1,6 +1,9 @@
 package javaFxGUI;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class JavaFxGuiDemo extends Application{
@@ -12,11 +15,24 @@ public class JavaFxGuiDemo extends Application{
 		
 		System.out.println("Hello World!");
 		
+		launch(args);
+		
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
 		
+		primaryStage.setTitle("Mobility Rechner");
+		
+		Button button = new Button("Berechnen");
+		
+		StackPane layout = new StackPane();
+		
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout, 300, 400);
+		primaryStage.setResizable(false);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 }
