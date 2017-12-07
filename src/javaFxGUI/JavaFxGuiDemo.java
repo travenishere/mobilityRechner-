fc4b213834d4125bca0843c9b150212ea53acaad
sevku <severin.kunz@gmail.com>
2017-12-07 19:29:40 +0100

    basic grid layout input form

--- src/javaFxGUI/JavaFxGuiDemo.java
+++ src/javaFxGUI/JavaFxGuiDemo.java
@@ -1,9 +1,12 @@
 package javaFxGUI;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -25,27 +28,61 @@ public class JavaFxGuiDemo extends Application {
 		
 		stage.setTitle("Mobility Rechner");
 		
-		Label label1 = new Label("Mobility Fahrt berechnen");
+		
+		// layout
+		GridPane inputLayout = new GridPane();
+		inputLayout.setPadding(new Insets(10, 10, 10, 10));
+		inputLayout.setVgap(8);
+		inputLayout.setHgap(10);
+		
+		// labels
+		Label startDateLabel = new Label("Startdatum");
+		GridPane.setConstraints(startDateLabel, 0, 0);
+		Label startTimeLabel = new Label("Startzeit");
+		GridPane.setConstraints(startTimeLabel, 2, 0);
+		Label endDateLabel = new Label("Enddatum");
+		GridPane.setConstraints(endDateLabel, 0, 1);
+		Label endTimeLabel = new Label("Endzeit");
+		GridPane.setConstraints(endTimeLabel, 2, 1);
+		
+		// text inputs
+		TextField startDateInput = new TextField("");
+		GridPane.setConstraints(startDateInput, 1, 0);
+		TextField startTimeInput = new TextField("");
+		GridPane.setConstraints(startTimeInput, 3, 0);
+		TextField endDateInput = new TextField("");
+		GridPane.setConstraints(endDateInput, 1, 1);
+		TextField endTimeInput = new TextField("");
+		GridPane.setConstraints(endTimeInput, 3, 1);
+		
+		// buttons
 		button = new Button("Berechnen");
+		GridPane.setConstraints(button, 3, 2);
 		button.setOnAction(e -> {
 			stage.setScene(resultScene);
 		});
-		
 		/* button.setOnAction(e -> System.out.println("Hello World!")}); */
 		
 		
+		inputLayout.getChildren().addAll(
+				startDateLabel, 
+				startDateInput, 
+				startTimeLabel, 
+				startTimeInput, 
+				endDateLabel,
+				endDateInput,
+				endTimeLabel,
+				endTimeInput,
+				button);
 		
-		VBox inputLayout = new VBox(20);
-		
-		inputLayout.getChildren().addAll(label1, button);
-		
-		inputScene = new Scene(inputLayout, 300, 400);
+
+		inputScene = new Scene(inputLayout, 600, 300);
 		
 		
 		Label label2 = new Label("Resultat");
 		StackPane resultLayout = new StackPane();
 		resultLayout.getChildren().add(label2);
-		resultScene = new Scene(resultLayout, 300, 400);
+		resultScene = new Scene(resultLayout, 600, 300);
 		
 		stage.setResizable(false);
 		stage.setScene(inputScene);
