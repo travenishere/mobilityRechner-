581bdd1084001bf0208a4449b7e6158a0379cdd9
sevku <severin.kunz@gmail.com>
2017-12-07 17:34:57 +0100

    preparation of javaFX demo

---
+++ src/javaFxGUI/JavaFxGuiDemo.java
@@ -0,0 +1,22 @@
+package javaFxGUI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFxGuiDemo extends Application{
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
